267ace0e0cf6c853301b86c06604a2aab3cc322d
Gonzalo Abruna <gonzalo@orgos.io>
2020-09-15 00:02:47 +0200

    Step 3. Styling your app using CSS

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: "gr-login",
   moduleId: module.id,
-  templateUrl: "./login/login.component.html"
+  templateUrl: "./login/login.component.html",
+  styleUrls: ["./login/login.component.css"]
 })
 export class AppComponent {}
---
+++ platforms/android/app/src/main/assets/app/bundle.7a469cf36d5da59025fc.hot-update.js
@@ -0,0 +1,37 @@
+webpackHotUpdate("bundle",{
+
+/***/ "./app.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AppComponent", function() { return AppComponent; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("@angular/core");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_angular_core__WEBPACK_IMPORTED_MODULE_0__);
+
+var AppComponent = /** @class */ (function () {
+    function AppComponent() {
+    }
+    AppComponent = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
+            selector: "gr-login",
+            template: __importDefault(__webpack_require__("./login/login.component.html")).default,
+            styles: [__importDefault(__webpack_require__("./login/login.component.css")).default]
+        })
+    ], AppComponent);
+    return AppComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./login/login.component.css":
+/***/ (function(module, exports) {
+
+module.exports = ""
+
+/***/ })
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9hcHAuY29tcG9uZW50LnRzIiwid2VicGFjazovLy8uL2xvZ2luL2xvZ2luLmNvbXBvbmVudC5jc3MiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBMEM7QUFRMUM7SUFBQTtJQUEyQixDQUFDO0lBQWYsWUFBWTtRQU54QiwrREFBUyxDQUFDO1lBQ1QsUUFBUSxFQUFFLFVBQVU7WUFFcEIsc0ZBQTJDOztTQUU1QyxDQUFDO09BQ1csWUFBWSxDQUFHO0lBQUQsbUJBQUM7Q0FBQTtBQUFIOzs7Ozs7OztBQ1J6QixtQiIsImZpbGUiOiJidW5kbGUuN2E0NjljZjM2ZDVkYTU5MDI1ZmMuaG90LXVwZGF0ZS5qcyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IENvbXBvbmVudCB9IGZyb20gXCJAYW5ndWxhci9jb3JlXCI7XG5cbkBDb21wb25lbnQoe1xuICBzZWxlY3RvcjogXCJnci1sb2dpblwiLFxuICBtb2R1bGVJZDogbW9kdWxlLmlkLFxuICB0ZW1wbGF0ZVVybDogXCIuL2xvZ2luL2xvZ2luLmNvbXBvbmVudC5odG1sXCIsXG4gIHN0eWxlVXJsczogW1wiLi9sb2dpbi9sb2dpbi5jb21wb25lbnQuY3NzXCJdXG59KVxuZXhwb3J0IGNsYXNzIEFwcENvbXBvbmVudCB7fSIsIm1vZHVsZS5leHBvcnRzID0gXCJcIiJdLCJzb3VyY2VSb290IjoiIn0=
