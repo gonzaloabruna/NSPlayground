00cb624998a0b7ee048fbb308529214843c5432d
Gonzalo Abruna <gonzalo@orgos.io>
2020-09-16 00:32:18 +0200

    Step 11. Customizing UI components

--- app/login/login.component.ts
+++ app/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { Page } from "tns-core-modules/ui/page";
 import { User } from "../shared/user/user.model";
 import { UserService } from "../shared/user/user.service";
 
@@ -10,14 +11,18 @@ import { UserService } from "../shared/user/user.service";
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   user: User;
   isLoggingIn = true;
 
-  constructor(private router: Router, private userService: UserService) {
+  constructor(private router: Router, private userService: UserService, private page: Page) {
     this.user = new User();
     this.user.email = "test1234@test.com";
     this.user.password = "test";
+  }
+
+  ngOnInit() {
+    this.page.actionBarHidden = true;
   }
 
   submit() {
