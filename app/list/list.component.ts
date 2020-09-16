c9c3492260fa08f0a5587869ab329aa345827e99
Gonzalo Abruna <gonzalo@orgos.io>
2020-09-16 23:08:28 +0200

    Step 12. Introducing NativeScript UI

--- app/list/list.component.ts
+++ app/list/list.component.ts
@@ -1,9 +1,25 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Grocery } from "../shared/grocery/grocery.model";
+import { GroceryService } from "../shared/grocery/grocery.service";
 
 @Component({
   selector: "gr-list",
   moduleId: module.id,
   templateUrl: "./list.component.html",
-  styleUrls: ["./list.component.css"]
+  styleUrls: ["./list.component.css"],
+  providers: [GroceryService]
 })
-export class ListComponent {}
+export class ListComponent implements OnInit {
+  groceryList: Array<Grocery> = [];
+
+  constructor(private groceryService: GroceryService) {}
+
+  ngOnInit() {
+    this.groceryService.load()
+      .subscribe((loadedGroceries: []) => {
+        loadedGroceries.forEach((groceryObject: Grocery) => {
+          this.groceryList.unshift(groceryObject);
+        });
+      });
+  }
+}
